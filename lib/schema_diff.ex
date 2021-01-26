3d42f8a301b08029520d49c487d7b5e0bb5a75a1
Dave Lucia <davelucianyc@gmail.com>
2021-01-26 09:10:14 -0500

    initial

---
+++ lib/schema_diff.ex
@@ -0,0 +1,6 @@
+defmodule SchemaDiff do
+  # diff <(jq -S . A.json) <(jq -S . B.json)
+  def diff do
+    :ok
+  end
+end
---
+++ test/schema_diff_test.exs
@@ -0,0 +1,8 @@
+defmodule SchemaDiffTest do
+  use ExUnit.Case
+  doctest SchemaDiff
+
+  test "greets the world" do
+    assert SchemaDiff.hello() == :world
+  end
+end
